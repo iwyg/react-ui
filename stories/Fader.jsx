33d19d0844d466aa47a213c2d0fd44e68037490d
iwyg <mail@thomas-appel.com>
2016-11-15 03:07:45 +0100

    update fader

--- stories/Fader.jsx
+++ stories/Fader.jsx
@@ -2,18 +2,32 @@ import React, {PropTypes} from 'react';
 import Base from 'components/base';
 import Slider from 'components/slider';
 import Label from 'components/label';
-import {ORIENT_VR} from 'components/constants'
+import {ORIENT_VR} from 'components/constants';
 import {toFixed} from 'lib/num';
 
-
+/**
+ *  linear to log conversion
+ */
 const valueToDb = (value, prec = 0.01, base = 10) => {
   return toFixed(10 * Math.log10((Math.pow(base, value) - 1) / (base - 1)), prec);
 };
 
+/**
+ * Returns a string representation of the current volume level.
+ *
+ * @param {Number} value
+ * @return {String}
+ */
 const getDb = (value) => {
-  return valueToDb(value / 100, .1, 5);
+  const val = valueToDb(value / 100, .1, 5);
+  return val === Number.NEGATIVE_INFINITY ? '-'
+    + String.fromCharCode(parseInt('221E', 16)) : val.toString();
 };
 
+/**
+ * A fader element
+ * @constructor
+ */
 export default class Fader extends React.Component {
   state = {
     value: 100,
@@ -27,7 +41,7 @@ export default class Fader extends React.Component {
    */
   onUpdate = (value) => {
     const db = getDb(value);
-    this.setState({value, db})
+    this.setState({value, db});
   }
 
   /**
@@ -55,3 +69,7 @@ export default class Fader extends React.Component {
     );
   }
 }
+
+Fader.propTypes = {
+  style: PropTypes.object
+};
